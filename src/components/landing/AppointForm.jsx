0067c7becb055ff6f822a2c6bbf50e9f07ca723b
nobledev89 <dpnh1989@gmail.com>
2024-08-15 09:12:31 +0100

    Updated form

--- src/components/landing/AppointForm.jsx
+++ src/components/landing/AppointForm.jsx
@@ -1,9 +1,8 @@
-import { useState } from "react"
-import { BiCalendarPlus } from "react-icons/bi";
-import './landing.css'
-import Container from 'react-bootstrap/Container'
-import Row from 'react-bootstrap/Row'
-import Col from 'react-bootstrap/Col'
+import { useState } from "react";
+import './landing.css';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
 
 const AppointForm = ({ onSendAppointment, lastId }) => {
   const clearData = {
@@ -13,6 +12,7 @@ const AppointForm = ({ onSendAppointment, lastId }) => {
     aptDate: "",
     aptTime: "",
   };
+
   let [toggleForm, setToggleForm] = useState(false);
   let [formData, setFormData] = useState(clearData);
 
@@ -37,135 +37,114 @@ const AppointForm = ({ onSendAppointment, lastId }) => {
     <div>
       <button
         onClick={() => setToggleForm(!toggleForm)}
-        className={`btn-appoint  ${
-          toggleForm ? "rounded-t-md" : "rounded-md"
-        }`}
+        className={`btn-appoint ${toggleForm ? "rounded-t-md" : "rounded-md"}`}
       >
         <div className="inline-block align-text-top"> 
           Book Appointment
         </div>
       </button>
       {toggleForm && (
-        <div className="border-r-2 border-b-2 border-l-2 border-light-blue-500 rounded-b-md pl-4 pr-4 pb-4">
-          <div className="sm:grid sm:grid-cols-3 sm:gap-4 sm:items-start  sm:pt-5">
-            <label
-              htmlFor="Email"
-              className="block text-sm font-medium text-gray-700 sm:mt-px sm:pt-2"
+        <div className="modal-overlay">
+          <div className="modal-content">
+            <span
+              className="close-btn"
+              onClick={() => setToggleForm(false)}
             >
-              Email
-            </label>
-            <div className="mt-1 sm:mt-0 sm:col-span-2">
-              <input
-                onChange={(event) => {
-                  setFormData({ ...formData, Email: event.target.value });
-                }}
-                type="email"
-                name="Email"
-                id="Email"
-                value={formData.Email}
-                required
-                className="max-w-lg block w-full shadow-sm focus:ring-indigo-500 focus:border-indigo-500 sm:max-w-xs sm:text-sm border-gray-300 rounded-md"
-              />
-            </div>
-          </div>
-
-          <div className="sm:grid sm:grid-cols-3 sm:gap-4 sm:items-start  sm:pt-5">
-            <label
-              htmlFor="Name"
-              className="block text-sm font-medium text-gray-700 sm:mt-px sm:pt-2"
-            >
-              Name
-            </label>
-            <div className="mt-1 sm:mt-0 sm:col-span-2">
-              <input
-                onChange={(event) => {
-                  setFormData({ ...formData, Name: event.target.value });
-                }}
-                required
-                type="text"
-                name="Name"
-                id="Name"
-                value={formData.Name}
-                className="max-w-lg block w-full shadow-sm focus:ring-indigo-500 focus:border-indigo-500 sm:max-w-xs sm:text-sm border-gray-300 rounded-md"
-              />
-            </div>
-          </div>
-
-          <div className="sm:grid sm:grid-cols-3 sm:gap-4 sm:items-start  sm:pt-5">
-            <label
-              htmlFor="aptDate"
-              className="block text-sm font-medium text-gray-700 sm:mt-px sm:pt-2"
-            >
-              Apt Date
-            </label>
-            <div className="mt-1 sm:mt-0 sm:col-span-2">
-              <input
-                required
-                onChange={(event) => {
-                  setFormData({ ...formData, aptDate: event.target.value });
-                }}
-                type="date"
-                name="aptDate"
-                id="aptDate"
-                value={formData.aptDate}
-                className="max-w-lg block w-full shadow-sm focus:ring-indigo-500 focus:border-indigo-500 sm:max-w-xs sm:text-sm border-gray-300 rounded-md"
-              />
-            </div>
-          </div>
-
-          <div className="sm:grid sm:grid-cols-3 sm:gap-4 sm:items-start  sm:pt-5">
-            <label
-              htmlFor="aptTime"
-              className="block text-sm font-medium text-gray-700 sm:mt-px sm:pt-2"
-            >
-              Apt Time
-            </label>
-            <div className="mt-1 sm:mt-0 sm:col-span-2">
-              <input
-                onChange={(event) => {
-                  setFormData({ ...formData, aptTime: event.target.value });
-                }}
-                type="time"
-                name="aptTime"
-                id="aptTime"
-                value={formData.aptTime}
-                className="max-w-lg block w-full shadow-sm focus:ring-indigo-500 focus:border-indigo-500 sm:max-w-xs sm:text-sm border-gray-300 rounded-md"
-              />
-            </div>
-          </div>
-
-          <div className="sm:grid sm:grid-cols-3 sm:gap-4 sm:items-start  sm:pt-5">
-            <label
-              htmlFor="aptNotes"
-              className="block text-sm font-medium text-gray-700 sm:mt-px sm:pt-2"
-            >
-              Appointment Notes
-            </label>
-            <div className="mt-1 sm:mt-0 sm:col-span-2">
-              <textarea
-                onChange={(event) => {
-                  setFormData({ ...formData, aptNotes: event.target.value });
-                }}
-                value={formData.aptNotes}
-                id="aptNotes"
-                name="aptNotes"
-                rows="3"
-                className="shadow-sm focus:ring-indigo-500 focus:border-indigo-500 mt-1 block w-full sm:text-sm border-gray-300 rounded-md"
-                placeholder="Detailed comments about the condition"
-              ></textarea>
-            </div>
-          </div>
-
-          <div className="pt-5">
-            <div className="flex justify-end">
-              <button
-                type="submit"
-                onClick={formDataPosted}
-                className="btn-appoint"
-              >
-                Submit
-              </button>
-            </div>
+              &times;
+            </span>
+            <h2>Book Appointment</h2>
+            <Container>
+              <Row>
+                <Col>
+                  <label htmlFor="Email">Email</label>
+                  <input
+                    onChange={(event) => {
+                      setFormData({ ...formData, Email: event.target.value });
+                    }}
+                    type="email"
+                    name="Email"
+                    id="Email"
+                    value={formData.Email}
+                    required
+                    className="text-field"
+                  />
+                </Col>
+              </Row>
+              <Row>
+                <Col>
+                  <label htmlFor="Name">Name</label>
+                  <input
+                    onChange={(event) => {
+                      setFormData({ ...formData, Name: event.target.value });
+                    }}
+                    required
+                    type="text"
+                    name="Name"
+                    id="Name"
+                    value={formData.Name}
+                    className="text-field"
+                  />
+                </Col>
+              </Row>
+              <Row>
+                <Col>
+                  <label htmlFor="aptDate">Apt Date</label>
+                  <input
+                    required
+                    onChange={(event) => {
+                      setFormData({ ...formData, aptDate: event.target.value });
+                    }}
+                    type="date"
+                    name="aptDate"
+                    id="aptDate"
+                    value={formData.aptDate}
+                    className="text-field"
+                  />
+                </Col>
+              </Row>
+              <Row>
+                <Col>
+                  <label htmlFor="aptTime">Apt Time</label>
+                  <input
+                    onChange={(event) => {
+                      setFormData({ ...formData, aptTime: event.target.value });
+                    }}
+                    type="time"
+                    name="aptTime"
+                    id="aptTime"
+                    value={formData.aptTime}
+                    className="text-field"
+                  />
+                </Col>
+              </Row>
+              <Row>
+                <Col>
+                  <label htmlFor="aptNotes">Appointment Notes</label>
+                  <textarea
+                    onChange={(event) => {
+                      setFormData({ ...formData, aptNotes: event.target.value });
+                    }}
+                    value={formData.aptNotes}
+                    id="aptNotes"
+                    name="aptNotes"
+                    rows="3"
+                    className="text-field"
+                    placeholder="Detailed comments about the condition"
+                  ></textarea>
+                </Col>
+              </Row>
+              <Row>
+                <Col>
+                  <button
+                    type="button"
+                    onClick={formDataPosted}
+                    className="submit-btn"
+                  >
+                    Submit
+                  </button>
+                </Col>
+              </Row>
+            </Container>
           </div>
         </div>
       )}
